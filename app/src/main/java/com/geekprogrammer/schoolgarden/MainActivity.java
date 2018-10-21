1ecc8790a1cd94ca9b470d9750814f6178555013
GeekGianca <giancarlos.cuello@gmail.com>
2018-10-20 23:17:20 -0500

    Initial commit

---
+++ app/src/main/java/com/geekprogrammer/schoolgarden/MainActivity.java
@@ -0,0 +1,57 @@
+package com.geekprogrammer.schoolgarden;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+            FragmentManager manager = getFragmentManager();
+            FragmentTransaction transaction = manager.beginTransaction();
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    fragment = new ObjetivesFragment();
+                    transaction.replace(R.id.frameLayout, fragment);
+                    transaction.addToBackStack(null);
+                    transaction.commit();
+                    return true;
+                case R.id.navigation_dashboard:
+                    fragment = new PlantsFragment();
+                    transaction.replace(R.id.frameLayout, fragment);
+                    transaction.addToBackStack(null);
+                    transaction.commit();
+                    return true;
+                case R.id.navigation_notifications:
+
+                    return true;
+                case R.id.navigation_garden:
+
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        MenuItem item = navigation.getMenu().getItem(0);
+        navigation.setSelectedItemId(item.getItemId());
+    }
+
+}
